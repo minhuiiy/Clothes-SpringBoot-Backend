b0a99d548add48cf0be43043d18dc9dd9029c73a
minhuyi <minhhuy5929@gmail.com>
2026-04-04 14:47:56 +0700

    chore: init spring boot project with maven

---
+++ src/main/java/com/clothes/backend/ClothesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clothes.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesBackendApplication.class, args);
+	}
+
+}
